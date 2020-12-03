ad7197ebf04ac6bf0e62e9fda02e828b8ee518cb
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-03 08:59:31 -0500

    Day 3 p2

--- 3.py
+++ 3.py
@@ -1,10 +1,27 @@
-tree_count = 0
+import math
+
+class Slope:
+    def __init__(self, right, down=1):
+        self.right = right
+        self.down  = down
+        self.h_pos = 0
+        self.v_pos = 0
+        self.trees = 0
+
+    def move(self, line):
+        if self.v_pos == 0:
+            if line[self.h_pos] == '#':
+                self.trees += 1
+            self.h_pos = (self.h_pos + self.right) % len(line)
+        self.v_pos = (self.v_pos + 1) % self.down
+        
+slopes = [Slope(1), Slope(3), Slope(5), Slope(7), Slope(1, 2)]
+
 with open('3.txt') as fp:
-    pos = 0
     for line in fp:
         line = line.rstrip()
-        if line[pos] == '#':
-            tree_count += 1
-        pos = (pos + 3) % len(line)
+        for slope in slopes:
+            slope.move(line)
 
-print(tree_count)
+result = math.prod(slope.trees for slope in slopes)
+print(result)

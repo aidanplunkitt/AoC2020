376642c7843b05edaad05c00a3ce1129b7c22fa1
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-02 22:37:31 -0500

    Day 1 part 2

--- 1.py
+++ 1.py
@@ -1,9 +1,17 @@
-s = set()
+def threesum(nums):
+    for i in range(len(nums)):
+        s = set()
+        a = nums[i]
+        for j in range(i + 1, len(nums)):
+            b = nums[j]
+            if nums[j] in s:
+                return a * b * (2020 - a - b)
+            else:
+                s.add(2020 - a - b)
+    return -1
+
+nums_ = []
 with open('1.txt') as fp:
-    for num in fp:
-        num = int(num)
-        if num in s:
-            print(num * (2020 - num))
-            break
-        else:
-            s.add(2020 - num)
+    nums_ = [int(num) for num in fp]
+
+print(threesum(nums_))

0fa5419b127d842209253c7566ad85d71f9203c6
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-06 09:22:09 -0500

    Day 6 p2

--- 6.py
+++ 6.py
@@ -1,15 +1,16 @@
 import string
 
 s = set()
+s.update(c for c in string.ascii_lowercase)
 group_sum = 0
 
 with open('6.txt') as fp:
     for line in fp:
         line = line.strip()
         if line:
-            s.update(c for c in line)
+            s.intersection_update(c for c in line)
         else:
             group_sum += len(s)
-            s.clear()
+            s.update(c for c in string.ascii_lowercase)
 
 print(group_sum + len(s))

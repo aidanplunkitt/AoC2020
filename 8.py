7fab7c49114d842323e66dfc4ec31f62e281a96f
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-08 10:20:41 -0500

    Day 8 p2

--- 8.py
+++ 8.py
@@ -1,16 +1,32 @@
 code = [line.strip() for line in open('8.txt')]
 
-acc = [0] # list to force global var
-def execute(addr):
-    instr, data = code[addr].split()
-    if instr == 'acc':
-        acc[0] += int(data)
+def execute(jmps_and_nops=[]):
+    visited = addr = acc = 0
+    while not ((1 << addr) & visited) and addr < len(code):
+        instr, data = code[addr].split()
+        if instr == 'acc':
+            acc += int(data)
+        else:
+            jmps_and_nops.append((addr, instr, data))
 
-    return addr + (int(data) if instr == 'jmp' else 1)
+        visited |= 1 << addr
+        addr += int(data) if instr == 'jmp' else 1
 
-visited = address = 0
-while not ((1 << address) & visited):
-    visited |= 1 << address
-    address = execute(address)
+    return addr, acc
 
-print(acc[0])
+# populate list of candidate ops to edit
+init_jmps_and_nops = []
+execute(init_jmps_and_nops)
+
+# bruteforce
+acc = 0
+swap = {'jmp':'nop', 'nop':'jmp'}
+for addr, instr, data in init_jmps_and_nops:
+    orig = code[addr]
+    code[addr] = ' '.join([swap[instr], data])
+    final_addr, acc = execute()
+    if final_addr == len(code):
+        break
+    code[addr] = orig
+
+print(acc)

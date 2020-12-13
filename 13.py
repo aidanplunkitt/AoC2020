9a700ebd3e169cff672cbc37eafea619c0ad51bc
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-13 15:02:40 -0500

    Day 13 p2 chinese remainder theorem solution

--- 13.py
+++ 13.py
@@ -1,9 +1,18 @@
+import math
+
 _, buses = [line.strip() for line in open('13.txt')]
-buses = [(pos, int(bus)) for pos, bus in enumerate(buses.split(',')) if bus != 'x']
-print(buses)
-time = 0
-while True:
-    if all([(time + pos) % bus == 0 for pos, bus in buses]):
-        print(time)
-        break
-    time += buses[0][1]
+buses = [(dt, int(bus)) for dt, bus in enumerate(buses.split(',')) if bus != 'x']
+
+# Fermat's little theorem
+def inverse_mod(a, b):
+    return pow(a, b-2, b)
+
+result = 0
+X = math.prod([bus[1] for bus in buses])
+# Chinese remainder theorem
+for dt, mod in buses:
+    a_k = (mod - dt) % mod
+    X_k = X // mod
+    result += a_k * X_k * inverse_mod(X_k, mod)
+
+print(result % X)

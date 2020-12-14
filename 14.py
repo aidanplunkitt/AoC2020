afac4ea1aa6801017f46b9870c0eefcc10fca101
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-14 09:16:29 -0500

    Day 14 p2

--- 14.py
+++ 14.py
@@ -1,11 +1,17 @@
-def apply_mask(num, mask):
+def apply_mask(addr, mask):
+    addrs = [addr]
     for i in range(len(mask)):
-        if mask[35-i] != 'X':
-            if mask[35-i] == '1':
-                num |= 1 << i
-            else:
-                num &= ~(1 << i)
-    return num
+        if mask[35-i] == '1':
+            addrs = [addr | (1 << i) for addr in addrs]
+        if mask[35-i] == 'X':
+            tmp = []
+            while addrs:
+                a = addrs.pop()
+                tmp.append(a & ~(1 << i)) # set X to 0
+                tmp.append(a |  (1 << i)) # set X to 1
+            addrs = tmp
+            
+    return addrs
 
 d= {}
 mask = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
@@ -15,6 +21,8 @@ with open('14.txt') as fp:
         if line[0] == 'mask':
             mask = line[1]
         else:
-            d[line[0]] = apply_mask(int(line[1]), mask)
+            addr = int(line[0][4:].rstrip(']'))
+            for a in apply_mask(addr, mask):
+                d[a] = int(line[1])
 
 print(sum(d.values()))

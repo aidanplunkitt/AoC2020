85bc5ec6dc9a6dcfdbad8e3fa2b4e161e077e3e9
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-15 09:33:55 -0500

    Day 15 p1 + p2

--- 15.py
+++ 15.py
@@ -1,59 +1,10 @@
-STARTING = [0,3,6]
+STARTING = [12,20,0,6,1,17,7]
+heard = {n: turn+1 for turn, n in enumerate(STARTING[:-1])}
 
-class LRU_Cache:
-    class Node:
-        def __init__(self, data):
-            self.data = data
-            self.prev = None
-            self.next = None
+last_spoken = STARTING[-1]
+for turn in range(len(STARTING), 30000000):
+    next_to_speak = 0 if last_spoken not in heard else turn - heard[last_spoken]
+    heard[last_spoken] = turn
+    last_spoken = next_to_speak
 
-    def __init__(self):
-        self.head, self.tail = self.Node(None), self.Node(None)
-        self.head.next, self.tail.prev = self.tail, self.head
-        self.map = {}
-
-    def __repr__(self):
-        nodes = ["dh"]
-        n = self.head.next
-        while n is not self.tail:
-            nodes.append(str(n.data))
-            n = n.next
-        nodes.append("dt")
-        return " -> ".join(nodes)
-
-    def insert(self, data):
-        age = 0
-        if data in self.map:
-            n = self.map[data]
-
-            # TODO make this O(1) time
-            p = self.tail
-            while p is not n:
-                p = p.prev
-                age += 1
-
-            # remove from cache
-            n.prev.next, n.next.prev = n.next, n.prev
-        else:
-            n = self.Node(data)
-            self.map[data] = n
-
-        # place in cache
-        n.prev, n.next = self.tail.prev, self.tail
-        self.tail.prev.next = n
-        self.tail.prev = n
-
-        return age
-
-lc = LRU_Cache()
-last = -1
-nth_spoken = 1
-for n in STARTING:
-    last = lc.insert(n)
-    nth_spoken += 1
-
-for i in range(2020 - len(STARTING) - 1):
-    last = lc.insert(last)
-    nth_spoken += 1
-
-print(nth_spoken, last)
+print(last_spoken)
